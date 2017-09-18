62b5620cc352d6485357cfac0ed60b05beeb72e5
eaccmk <eaccmk@users.noreply.github.com>
2017-09-18 15:37:33 +0530

    create HashSet.java
    Basic HashSet Implementation

---
+++ com/sololearn/playground/code/HashSet.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        HashSet<String> set = new HashSet<String>();
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        set.add("eaccmk");
+        System.out.println(set);
+        System.out.println(set.get(3));
+    }
+}
